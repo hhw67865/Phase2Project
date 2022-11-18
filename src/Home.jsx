290e9feb13453ebea82ced04409d5efbdd1fb37b
Henry Wu <hhw67865@gmail.com>
2022-11-18 11:04:33 -0500

    h1 color black

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,7 @@ function Home ({deleteRoutine,workoutData, routineList, setRoutineList, handleAd
     return (
         <>
             <img className='imglogo' src="https://images-platform.99static.com/Lw8CyZQrGrnlyPa-QHw8p4Tn7Nw=/200x200:1800x1800/500x500/top/smart/99designs-contests-attachments/121/121893/attachment_121893514" alt="logo" />
-            <h1 className="logo">My Workout Routine</h1>
+            <h1 style={{color: "black"}} className="logo">My Workout Routine</h1>
             <Focus setFocus={setFocus} focus={focus} />
             <WorkoutCreation setSearchQuery={setSearchQuery} deleteRoutine={deleteRoutine} handleAddRoutine={handleAddRoutine} setRoutineList={setRoutineList} routineList={routineList} workoutData= {workoutData} focus = {focus}/>
         </>
